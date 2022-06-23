8c3d481ca524f33f8bbd12a5c1fabb6f931d9ed4
prema <atia.aiasha.1998@gmail.com>
2022-06-23 21:39:35 +0600

    first commit

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,22 @@
+<div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
+        <div class="nano">
+            <div class="nano-content">
+                <ul>
+                    <div class="logo"><a href="index.html">
+                            <!-- <img src="assets/images/logo.png" alt="" /> --><span>Focus</span></a></div>
+                    <li class="label">Main</li>
+                    <li><a href="{{route('home')}}"><i class="fa fa-dashboard"></i> Dashboard </a></li> 
+                    <li><a class="sidebar-sub-toggle"><i class="ti-bar-chart-alt"></i> Employee <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                        <ul>
+                            <li><a href="#">Add Employee</a></li>
+                            <li><a href="#">View Employee</a></li>
+                            
+                        </ul>
+                    </li>
+                    
+                    <li><a><i class="ti-close"></i> Logout</a></li>
+                </ul>
+            </div>
+        </div>
+</div>
---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+@include('admin.includes.style')
+</head>
+
+<body>
+<!-- sidebar start -->
+@include('admin.includes.sidebar')
+    <!-- /# sidebar -->
+@include('admin.includes.header')
+    
+
+
+    <div class="content-wrap">
+        <div class="main">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-8 p-r-0 title-margin-right">
+                        <div class="page-header">
+                            <div class="page-title">
+                                <h1>Hello, <span>Welcome Here</span></h1>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /# column -->
+                    <div class="col-lg-4 p-l-0 title-margin-left">
+                        <div class="page-header">
+                            <div class="page-title">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                                    <li class="breadcrumb-item active">Home</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /# column -->
+                </div>
+                <!-- /# row -->
+                <section id="main-content">
+
+                @yield('content')
+
+                    <!-- /#row -->
+                    
+                  
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="footer">
+                                <p>2018 © Admin Board. - <a href="#">example.com</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <!-- jquery vendor -->
+    @include('admin.includes.scriptjs')
+</body>
+
+</html>
